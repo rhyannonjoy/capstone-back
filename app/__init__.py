19783c490e024a1885435dcc2fcd26628fb31752
Rhyannon <rhyannonjoy@gmail.com>
2022-02-05 14:42:50 -0700

    initialized db, created model Question

--- app/__init__.py
+++ app/__init__.py
@@ -22,19 +22,14 @@ def create_app():
     from app.models.question import Question
     
    
-
     # Setup DB
     db.init_app(app)
     migrate.init_app(app, db)
     
 
     # Register Blueprints here
-    from .routes import boards_bp
-    app.register_blueprint(boards_bp)
-
-    from .routes import cards_bp
-    app.register_blueprint(cards_bp)
-
+    from .routes import questions_bp
+    app.register_blueprint(questions_bp)
 
     #CORS(app)
     return app
--- app/models/question.py
+++ app/models/question.py
@@ -1,11 +1,23 @@
 from app import db
 
 class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    unchecked_question = db.Column(db.String)
-    checked_question = db.Column(db.String)
+    question_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    unedited_question = db.Column(db.String)
+    edited_question = db.Column(db.String)
     answer = db.Column(db.String)
-    #date = db.Column(#need to get a date in here)
+    date = db.Column(db.DateTime, nullable=True)
     keywords = db.Column(db.String)
     topic = db.Column(db.String)
     research = db.Column(db.String)
+
+    def to_json(self):
+        return {
+                "id": self.question_id,
+                "unedited_question" : self.unedited_question,
+                "edited_question" : self.edited_question,
+                "answer" : self.answer,
+                "date" : False if self.completed_at == None else True,
+                "keywords" : self.keywords,
+                "topic" : self.topic,
+                "research" : self.research
+        }
