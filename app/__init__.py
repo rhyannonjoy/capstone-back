bd0e0409bc24b995e6d25a92132ec3d868a53598
Rhyannon <rhyannonjoy@gmail.com>
2022-02-06 11:25:29 -0700

    installed flask-cors, updated requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -3,24 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
-#from flask_cors import CORS
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
 load_dotenv()
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    if test_config is None:
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
-    else:
-        app.config["TESTING"] = True
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI")
 
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
@@ -36,5 +35,5 @@ def create_app(test_config=None):
     from .routes import questions_bp
     app.register_blueprint(questions_bp)
 
-    #CORS(app)
+    CORS(app)
     return app
